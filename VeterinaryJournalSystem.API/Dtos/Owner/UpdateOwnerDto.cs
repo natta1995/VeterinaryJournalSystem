cfd2946de784b1ca3996b876f315806d6ec68d0d
natta1995 <natalie.hallerdal@yahoo.se>
2026-05-28 15:38:44 +0200

    Implemented Owner API with full CRUD functionality

---
+++ VeterinaryJournalSystem.API/Dtos/Owner/UpdateOwnerDto.cs
@@ -0,0 +1,15 @@
+﻿namespace VeterinaryJournalSystem.API.Dtos.Owner
+{
+    public class UpdateOwnerDto
+    {
+  
+            public string FullName { get; set; } = string.Empty;
+
+            public string PhoneNumber { get; set; } = string.Empty;
+
+            public string PersonalNumber { get; set; } = string.Empty;
+
+            public string? Comment { get; set; }
+     }
+    
+}
